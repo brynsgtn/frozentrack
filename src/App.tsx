f0928d0574ee100beae23c659999a23cff58a74d
brynsgtn <bryansuguitan17@gmail.com>
2025-12-30 09:15:56 -0600

    Add delete modal

--- src/App.tsx
+++ src/App.tsx
@@ -38,6 +38,7 @@ function App() {
 
   const [viewModal, setViewModal] = useState<boolean>(false);
   const [editModal, setEditModal] = useState<boolean>(false);
+  const [deleteModal, setDeleteModal] = useState<boolean>(false);
 
 
   const handleAddItem = (e: React.FormEvent<HTMLFormElement>): void => {
@@ -323,11 +324,7 @@ function App() {
                 className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600"
                 onClick={() => {
                   if (selectedIndex === null) return
-
-                  if (confirm('Delete this item?')) {
-                    handleDeleteItem(selectedIndex)
-                    setViewModal(false)
-                  }
+                  setDeleteModal(true)
                 }}
 
               >
@@ -459,6 +456,44 @@ function App() {
           </div>
         </div>
       )}
+
+      {deleteModal && selectedItem && (
+        <div className="fixed inset-0 bg-black/40 flex items-center justify-center">
+          <div className="bg-white px-4 py-6 rounded-xl shadow-md w-full max-w-sm">
+            <h2 className="text-xl font-bold mb-2 text-red-600">
+              Delete Item
+            </h2>
+
+            <p className="text-gray-600 mb-4">
+              Are you sure you want to delete
+              <span className="font-semibold"> {selectedItem.name}</span>?
+              This action cannot be undone.
+            </p>
+
+            <div className="flex justify-end gap-2">
+              <button
+                className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md hover:bg-gray-400"
+                onClick={() => setDeleteModal(false)}
+              >
+                Cancel
+              </button>
+
+              <button
+                className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600"
+                onClick={() => {
+                  if (selectedIndex === null) return
+                  handleDeleteItem(selectedIndex)
+                  setDeleteModal(false)
+                  setViewModal(false)
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
 
 
 
